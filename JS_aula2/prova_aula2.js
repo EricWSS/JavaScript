e10220dd438cac4a2c7c47b3dae8f0595f2782bc
EricWSS <129308605+EricWSS@users.noreply.github.com>
2023-08-01 17:03:37 -0300

    Prova 02 de Javascript da Infinity School.
    Crie dois programas que imprimam a sequência de números de 0 a 9 utilizando um laço de repetição while e um que utilize um laço de repetição for. Cada número deve ser impresso em uma nova linha. 
    
    Deve ser feito um programa utilizando o laço de repetição while e um programa utilizando o laço de repetição for que faça oque pede a questão.

---
+++ JS_aula2/prova_aula2.js
@@ -0,0 +1,12 @@
+/* Crie dois programas que imprimam a sequência de números de 0 a 9 utilizando um laço de repetição while e um que utilize um laço de repetição for. Cada número deve ser impresso em uma nova linha. 
+
+Deve ser feito um programa utilizando o laço de repetição while e um programa utilizando o laço de repetição for que faça oque pede a questão. */
+
+for (x=0;x<10;x++){
+    console.log(x)
+}
+let y = 0
+while (y<10){
+    console.log(y)
+    y++
+}
