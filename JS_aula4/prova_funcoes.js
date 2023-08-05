23a8c2a083729f93809b11fc5ec90f17c132fdfd
EricWSS <129308605+EricWSS@users.noreply.github.com>
2023-08-05 12:50:46 -0300

    Prova de funções da Infinity Shcool
    Crie uma função chamada calculaAreaRetangulo que recebe dois parâmetros: largura e altura. A função deve calcular a área do retângulo e retornar o resultado.

---
+++ JS_aula4/prova_funcoes.js
@@ -0,0 +1,9 @@
+/* Crie uma função chamada calculaAreaRetangulo que recebe dois parâmetros: largura e altura. A função deve calcular a área do retângulo e retornar o resultado. */
+
+function calculaAreaRetangulo (lar,alt){
+    let area = lar * alt
+    return area
+}
+
+let retangulo = calculaAreaRetangulo(2,4)
+console.log(retangulo)
